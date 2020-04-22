1d0eaa70837156da88854fc2a9be5556fee6589e
sandrachoy <64128624+sandrachoy@users.noreply.github.com>
2020-04-22 22:21:03 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,38 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+
+    int noofInt = in.nextInt();
+
+    ArrayList<Integer> store = new ArrayList<>();
+    ArrayList<Integer> count = new ArrayList<>();
+    store.add(0);
+    count.add(0);
+
+    int mode = 0;
+    
+    for(int i = 0; i < noofInt; i++){
+      int no = in.nextInt();
+      store.add(no);
+      for(int j = 0; j < store.size(); j++){
+        if(no == store.get(j)){
+            count.set(j,count.get(j)+1);
+            break;
+        }
+        else{
+          count.add(1);
+          break;
+        }
+      }      
+    }
+    
+    for(int x = 0; x < store.size(); x++){
+      if(count.get(x) > mode){
+        mode = store.get(x);
+      }
+    }
+
+    System.out.println(mode);
     
   }
 }
