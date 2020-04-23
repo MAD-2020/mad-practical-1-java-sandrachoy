7fda6c3a4827959138e3ca63006fde9429958936
sandrachoy <64128624+sandrachoy@users.noreply.github.com>
2020-04-23 12:17:23 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,11 +11,13 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    //asks user to enter an integer
     System.out.print("Enter integer: ");
     
+    //reads the integer entered by user
     int no = in.nextInt();
     
+    //prints the result to integer multiplied by itself
     System.out.println(no*no);
     
   }
