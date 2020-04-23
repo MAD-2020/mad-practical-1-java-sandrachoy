e182e2a0011072eae8e2d30d33111ded26a8220e
sandrachoy <64128624+sandrachoy@users.noreply.github.com>
2020-04-23 12:15:48 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -20,11 +20,13 @@ public class Question4
     Scanner in = new Scanner(System.in);
 
     int no = in.nextInt();
-
+    //loop the number of times that is entered
     for(int i=no; i > 0; i--){
+      //loop while x is lesser than i, with each loop increase the number of *
       for(int x=0; x < i; x++){
         System.out.print("*");
       }
+      //allows for the * to be printed on a new line each loop
       System.out.println();
     }
     
